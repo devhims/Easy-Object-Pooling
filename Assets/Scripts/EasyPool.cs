09c58a59ed35e739744d5c610d91cbd71e0aa813
Himanshu Gupta <thinktank.himanshu@gmail.com>
2020-08-21 19:04:30 +0530

    included the option to declare pool size in the inspector

--- Assets/Scripts/EasyPool.cs
+++ Assets/Scripts/EasyPool.cs
@@ -8,9 +8,12 @@ namespace Hims.Arsenal
     public class EasyPool : MonoBehaviour
     {
         [SerializeField] GameObject[] prefabsToPool;
+        [SerializeField] int poolSize = 25;
         [SerializeField] bool randomise = false;
+
         Transform thisTransform;
         int currentIndex;
+        int prefabIndex;
 
         // array to store pooled objects
         GameObject[] pooledObjects;
@@ -28,19 +31,21 @@ namespace Hims.Arsenal
             Instance = this;
 
             thisTransform = transform;
-            pooledObjects = new GameObject[prefabsToPool.Length];
+            pooledObjects = new GameObject[poolSize];
             AddObjectsToPool();
         }
 
         // instantiate and add objects to pool
         void AddObjectsToPool()
         {
-            for (int i = 0; i < prefabsToPool.Length; i++)
+            for (int i = 0; i < poolSize; i++)
             {
-                GameObject go = Instantiate(prefabsToPool[i]);
+                prefabIndex %= prefabsToPool.Length;
+                GameObject go = Instantiate(prefabsToPool[prefabIndex]);
                 go.SetActive(false);
                 go.transform.parent = thisTransform;
                 pooledObjects[i] = go;
+                prefabIndex++;
             }
         }
 
